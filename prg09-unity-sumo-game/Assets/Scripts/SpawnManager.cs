022ef6e64356cac4d4d42d8f090d41443ba15ef9
Bram van Oosterhout <bls.place@gmail.com>
2022-03-10 13:27:31 +0100

    Finished

--- prg09-unity-sumo-game/Assets/Scripts/SpawnManager.cs
+++ prg09-unity-sumo-game/Assets/Scripts/SpawnManager.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 11.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        // create a new enemyPrefab, use GenerateSpawnPosition
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(1);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    void SpawnEnemyWave(int amountOfEnemies)
+    {
+        for (int i = 0; i < amountOfEnemies; i++)
+        {
+            // create a new enemyPrefab, use GenerateSpawnPosition
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
 
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
